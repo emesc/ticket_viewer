5e05825767f34ffe8365c5a61645de0809c4c3ef
emesc <em@curlycoder.me>
2017-07-17 13:25:07 +1000

    Refactored spec for viewer
    Created methods for common actions in the viewer spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,17 @@
 Dir["./spec/support/**/*.rb"].each { |f| require f }
+
+def fake_user_input(*args)
+  allow(Readline).to receive(:readline).and_return(*args)
+end
+
+def capture_stdout(&block)
+  old_stdout = $stdout.dup
+  fake = StringIO.new
+  $stdout = fake
+  begin
+    yield
+  ensure
+    $stdout = old_stdout
+  end
+  fake.string
+end
