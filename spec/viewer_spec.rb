c817c4ef4c36a74cc5fba273a42b66fc5cd69a6d
emesc <em@curlycoder.me>
2017-07-17 13:01:31 +1000

    Added an example for a closing message

--- spec/viewer_spec.rb
+++ spec/viewer_spec.rb
@@ -4,9 +4,18 @@ describe Viewer do
   let(:viewer) { Viewer.new }
 
   describe "#connect" do
-    it "prints out an opening greeting" do
+    it "prints out an opening message" do
       allow(Readline).to receive(:readline).and_return('quit')
       expect{ viewer.connect }.to output(/Welcome/).to_stdout
+    end
+  end
+
+  describe "#do_command" do
+    context "with quit command" do
+      it "prints out a closing message and exits" do
+        allow(Readline).to receive(:readline).and_return('quit')
+        expect{ viewer.connect }.to output(/Goodbye/).to_stdout
+      end
     end
   end
 end
