21d57f40f001f02c0483a202d172af70fc130513
emesc <em@curlycoder.me>
2017-07-17 13:26:28 +1000

    Added a method for viewing menu
    Refactored to use methods from spec_helper

--- spec/viewer_spec.rb
+++ spec/viewer_spec.rb
@@ -4,16 +4,24 @@ describe Viewer do
   let(:viewer) { Viewer.new }
 
   describe "#connect" do
-    it "prints out an opening message" do
-      allow(Readline).to receive(:readline).and_return('quit')
+    it "outputs an opening message" do
+      fake_user_input("quit")
       expect{ viewer.connect }.to output(/Welcome/).to_stdout
     end
   end
 
   describe "#do_command" do
+    context "with menu command" do
+      it "outputs a list of valid commands" do
+        fake_user_input("menu", "quit")
+        output = capture_stdout { viewer.connect }
+        expect(output).to include("*Type 'next'      to view the next page of tickets")
+      end
+    end
+
     context "with quit command" do
-      it "prints out a closing message and exits" do
-        allow(Readline).to receive(:readline).and_return('quit')
+      it "outputs a closing message and exits" do
+        fake_user_input("quit")
         expect{ viewer.connect }.to output(/Goodbye/).to_stdout
       end
     end
