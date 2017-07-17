0785eb56d67dfe0b883cf3ae619cfad050def9a1
emesc <em@curlycoder.me>
2017-07-17 22:20:10 +1000

    Added an example for ticket table header when next is called
    Used vcr cassette as response for listing next pages
    Temporarily modified other user inputs not to use an api connection

--- spec/viewer_spec.rb
+++ spec/viewer_spec.rb
@@ -6,7 +6,7 @@ describe Viewer do
   describe "#connect" do
     it "outputs an opening message" do
       fake_user_input("quit")
-      expect{ viewer.connect }.to output(/Welcome/).to_stdout
+      expect{ viewer.introduction }.to output(/Welcome/).to_stdout
     end
   end
 
@@ -14,15 +14,32 @@ describe Viewer do
     context "with menu command" do
       it "outputs a list of valid commands" do
         fake_user_input("menu", "quit")
-        output = capture_stdout { viewer.connect }
+        output = capture_stdout{ viewer.menu }
         expect(output).to include("*Type 'next'      to view the next page of tickets")
+      end
+    end
+
+    context "with next command" do
+      VCR.use_cassette("ticket results") do
+        vcr_viewer = Viewer.new
+        output = capture_stdout { vcr_viewer.connect }
+        
+        it "outputs the next list of tickets" do
+          fake_user_input("next", "quit")
+          tickets = output.split("\n")
+          expect(tickets[4]).to match(/Showing page 1/)
+          expect(tickets[5]).to match(/Type 'menu' to view options or 'quit' to exit/)
+          expect(tickets[6]).to eq("-" * 120)
+          expect(tickets[7]).to match(/^\s{2}ID\s|\sSubject\s{54}|\sRequester\s{6}|\sCreated\son\s{21}$/)
+          expect(tickets[8]).to eq("-" * 120)
+        end
       end
     end
 
     context "with quit command" do
       it "outputs a closing message and exits" do
         fake_user_input("quit")
-        expect{ viewer.connect }.to output(/Goodbye/).to_stdout
+        expect{ viewer.conclusion }.to output(/Goodbye/).to_stdout
       end
     end
   end
