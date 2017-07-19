fcb506e3789f5f194d9f90ccd3097fe3b234ac61
emesc <em@curlycoder.me>
2017-07-19 15:03:11 +1000

    Added method for suppressing outputs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,3 +12,13 @@ def capture_stdout(&block)
   ensure
     $stdout = old_stdout
 end
+
+def silence_output
+  # Store the original stderr and stdout in order to restore them later
+  @original_stderr = $stderr
+  @original_stdout = $stdout
+
+  # Redirect stderr and stdout
+  $stderr = File.new(File.join(File.dirname(__FILE__), 'dev', 'null.txt'), 'w+')
+  $stdout = File.new(File.join(File.dirname(__FILE__), 'dev', 'null.txt'), 'w+')
+end
