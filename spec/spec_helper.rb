1df8d04c8cae68e2fe9e9767ef0eb32c62a58109
emesc <em@curlycoder.me>
2017-07-21 18:50:16 +1000

    Removed unused methods

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,5 @@
 Dir["./spec/support/**/*.rb"].each { |f| require f }
 
-def fake_user_input(*args)
-  allow(Readline).to receive(:readline).and_return(*args)
-end
-
 def capture_stdout(&block)
   old_stdout = $stdout.dup
   $stdout = fake = StringIO.new
@@ -11,14 +7,4 @@ def capture_stdout(&block)
   fake.string
   ensure
     $stdout = old_stdout
-end
-
-def silence_output
-  # Store the original stderr and stdout in order to restore them later
-  @original_stderr = $stderr
-  @original_stdout = $stdout
-
-  # Redirect stderr and stdout
-  $stderr = File.new(File.join(File.dirname(__FILE__), 'dev', 'null.txt'), 'w+')
-  $stdout = File.new(File.join(File.dirname(__FILE__), 'dev', 'null.txt'), 'w+')
 end
